497221e28dc9f6957e3892b2d06703c77a11858e
jfocha <jfocha@gmail.com>
2021-09-15 23:04:22 -0700

    added comment functionality

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -4,6 +4,14 @@ const pizzaController = {
     // get all pizzas
     getAllPizza(req, res) {
         Pizza.find({})
+            // To populate a field, just chain the .populate() method onto your query, passing in an object with the key path plus the value of the field you want populated.
+            .populate({
+                path: 'comments',
+                // Note that we also used the select option inside of populate(), so that we can tell Mongoose that we don't care about the __v field on comments either. The minus sign - in front of the field indicates that we don't want it to be returned. If we didn't have it, it would mean that it would return only the __v field.
+                select: '-__v'
+            })
+            .select('-__v')
+            .sort({ _id: -1 })
             .then(dbPizzaData => res.json(dbPizzaData))
             .catch(err => {
                 console.log(err);
@@ -14,19 +22,23 @@ const pizzaController = {
     // get one pizza by id
     getPizzaById({ params }, res) {
         Pizza.findOne({ _id: params.id })
-            .then(dbPizzaData => {
-                // If no pizza is found, send 404
-                if (!dbPizzaData) {
-                    res.status(404).json({ message: 'No pizza found with this id!' });
-                    return;
-                }
-                res.json(dbPizzaData);
-            })
-            .catch(err => {
-                console.log(err);
-                res.status(400).json(err);
-            });
-    },
+          .populate({
+            path: 'comments',
+            select: '-__v'
+          })
+          .select('-__v')
+          .then(dbPizzaData => {
+            if (!dbPizzaData) {
+              res.status(404).json({ message: 'No pizza found with this id!' });
+              return;
+            }
+            res.json(dbPizzaData);
+          })
+          .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+          });
+      },
 
     // createPizza
     createPizza({ body }, res) {
