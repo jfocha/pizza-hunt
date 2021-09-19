ed12b83342292243e539bc0d5adca91b876690df
jfocha <jfocha@gmail.com>
2021-09-19 15:56:03 -0700

    added discussions

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,10 +1,21 @@
 const router = require('express').Router();
-const { addComment, removeComment } = require('../../controllers/comment-controller');
+const {
+    addComment,
+    removeComment,
+    addReply,
+    removeReply
+} = require('../../controllers/comment-controller');
 
 // /api/comments/<pizzaId>
 router.route('/:pizzaId').post(addComment);
 
 // /api/comments/<pizzaId>/<commentId>
-router.route('/:pizzaId/:commentId').delete(removeComment);
+router
+    .route('/:pizzaId/:commentId')
+    // This is a PUT route, instead of a POST, because technically we're not creating a new reply resource. Instead, we're just updating the existing comment resource.
+    .put(addReply)
+    .delete(removeComment);
+
+router.route('/:pizzaId/:commentId/:replyId').delete(removeReply);
 
 module.exports = router;
